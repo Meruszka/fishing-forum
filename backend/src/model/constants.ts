564a9b25f9916d7b36895eaa1b03837bbd8749e2
jodua <teqoox@gmail.com>
2024-01-15 23:37:37 +0100

    :pensive: Add contants for models

---
+++ backend/src/model/constants.ts
@@ -0,0 +1,18 @@
+const GEAR_KINDS = {
+    WEDKA: 'Wędka',
+    KOLOWROTEK: 'Kołowrotek',
+    PRZYNETA: 'Przynęta',
+    ZYLKA: 'Żyłka',
+    INNE: 'Inne',
+}
+
+const FISHING_SPOT_TYPES = {
+    JEZIORO: 'Jezioro',
+    RZEKA: 'Rzeka',
+    STAW: 'Staw',
+    INNE: 'Inne',
+}
+
+const POST_TYPES = {
+    POST: 'Post',
+}
