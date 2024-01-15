862bc49a7f7885438addca6018efc538b08fd31f
jodua <teqoox@gmail.com>
2024-01-15 22:24:45 +0100

    Add friends adding logic

--- backend/src/model/Friend.model.ts
+++ backend/src/model/Friend.model.ts
@@ -1,4 +1,12 @@
-import { Schema, model } from 'mongoose'
+import { ObjectId, Schema, model } from 'mongoose'
+
+interface IFriend {
+    friend: {
+        username: string
+        _id: ObjectId
+    }
+    since: Date
+}
 
 const FriendSchema = new Schema({
     friend: { type: Schema.Types.ObjectId, ref: 'User' },
@@ -8,3 +16,5 @@ const FriendSchema = new Schema({
 const Friend = model('Friend', FriendSchema)
 
 export default Friend
+
+export { IFriend }
