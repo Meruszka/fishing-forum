4ccc98a1a8a9f9dd19cf7ab8e44f21e138ee7416
Szymon <merski70@gmail.com>
2024-01-17 23:32:50 +0100

    :traffic_light: icons installed, scrollbar; work in progress with fishing spots

--- frontend/src/common/buttonCustom/buttonCustom.component.tsx
+++ frontend/src/common/buttonCustom/buttonCustom.component.tsx
@@ -7,10 +7,11 @@ interface ButtonCustomProps {
   size?: string;
   children?: React.ReactNode;
   disabled?: boolean;
+  className?: string;
 }
 
 const ButtonCustom = (props: ButtonCustomProps): ReactElement => {
-  const { label, onClick, color, size } = props;
+  const { label, onClick, color, size, className } = props;
   const buttonStyles = `bg-${color}-500 text-gray-700 px-4 py-2 rounded ${
     size === "small" ? "text-sm" : size === "large" ? "text-lg" : "text-base"
   } hover:bg-${color}-600 focus:outline-none focus:ring focus:border-${color}-700 disabled:opacity-50 disabled:cursor-not-allowed`;
@@ -18,7 +19,7 @@ const ButtonCustom = (props: ButtonCustomProps): ReactElement => {
   return (
     <button
       onClick={onClick}
-      className={buttonStyles}
+      className={className ? className : buttonStyles}
       disabled={props.disabled}
     >
       {label} {props.children}
---
+++ frontend/src/components/fishingSpots/fishingSpots.service.ts
@@ -0,0 +1,27 @@
+import { ApiClient } from "../../common/apiClient";
+import { FishingSpot } from "../../providers/currentUser/currentUser.type";
+
+export interface FishingSpotDTO {
+  name: string,
+  longitude: number,
+  latitude: number,
+  description: string,
+  rating: number,
+  type: string,
+  image: string,
+}
+
+export const newFishingSpotREST = async (apiClient: ApiClient, fishingSpot: FishingSpotDTO): Promise<void> => {
+  const response = await apiClient.post<void>('/fishingSpot', fishingSpot);
+  return response.data;
+}
+
+export const getFishingSpotsREST = async (apiClient: ApiClient): Promise<FishingSpot[]> => {
+  const response = await apiClient.get<FishingSpot[]>('/fishingSpot');
+  return response.data;
+}
+
+export const deleteFishingSpotREST = async (apiClient: ApiClient, id: string): Promise<void> => {
+  const response = await apiClient.delete<void>(`/fishingSpot/${id}`);
+  return response.data;
+}
