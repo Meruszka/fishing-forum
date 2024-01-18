40eaed448901ee965b4d21fc20d6337f2da70c92
jodua <teqoox@gmail.com>
2024-01-18 01:43:00 +0100

    :yum: Add fishingSpot validation

--- backend/src/model/FishingSpot.model.ts
+++ backend/src/model/FishingSpot.model.ts
@@ -1,4 +1,5 @@
 import { Schema, model } from 'mongoose'
+import { z } from 'zod'
 
 const FishingSpotSchema = new Schema({
     name: String,
@@ -11,6 +12,17 @@ const FishingSpotSchema = new Schema({
     author: { type: Schema.Types.ObjectId, ref: 'User' },
 })
 
+const FishingSpotValidator = z.object({
+    name: z.string().min(1).max(255),
+    longitude: z.number().min(-180).max(180),
+    latitude: z.number().min(-90).max(90),
+    description: z.string().min(1).max(255),
+    rating: z.number().min(0).max(5),
+    type: z.string().min(1).max(255),
+    image: z.string().min(1).max(512),
+})
+
 const FishingSpot = model('FishingSpot', FishingSpotSchema)
 
 export default FishingSpot
+export { FishingSpotValidator }
